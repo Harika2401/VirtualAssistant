d1801c7118c7dac417d9b07c1d04ce01f27422d4
Harika2401 <harikavv2401@gmail.com>
2025-12-29 21:41:56 +0530

    Update Card.jsx

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from "react";
-import { userDataContext } from "../context/userContext";
+import { userDataContext } from "../context/UserContext";
 
 function Card({image}){
  const{serverUrl,userData,setUserData,backendImage,setBackendImage,frontendImage,setFrontendImage,selectedImage,setSelectedImage}=useContext(userDataContext)
